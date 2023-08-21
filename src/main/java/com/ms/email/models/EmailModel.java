25cfb1bfd7d530eb8ede35a09aefa3540d364d6a
Denilson <denilsonrockmetal@hotmail.com>
2023-08-20 23:26:29 -0300

    first commit

---
+++ src/main/java/com/ms/email/models/EmailModel.java
@@ -0,0 +1,130 @@
+package com.ms.email.models;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import com.ms.email.enums.StatusEmail;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "TB_EMAIL")
+public class EmailModel {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long emailId;
+	private String ownerRef;
+	private String emailFrom;
+	private String emailTo;
+	private String subject;
+	@Column(columnDefinition = "TEXT")
+	private String text;
+	private LocalDateTime sendDateEmail;
+	private StatusEmail statusEmail;
+	
+	public EmailModel(Long emailId, String ownerRef, String emailFrom, String emailTo, String subject, String text,
+			LocalDateTime sendDateEmail, StatusEmail statusEmail) {
+		super();
+		this.emailId = emailId;
+		this.ownerRef = ownerRef;
+		this.emailFrom = emailFrom;
+		this.emailTo = emailTo;
+		this.subject = subject;
+		this.text = text;
+		this.sendDateEmail = sendDateEmail;
+		this.statusEmail = statusEmail;
+	}
+
+	public EmailModel() {}
+
+	public Long getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(Long emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getOwnerRef() {
+		return ownerRef;
+	}
+
+	public void setOwnerRef(String ownerRef) {
+		this.ownerRef = ownerRef;
+	}
+
+	public String getEmailFrom() {
+		return emailFrom;
+	}
+
+	public void setEmailFrom(String emailFrom) {
+		this.emailFrom = emailFrom;
+	}
+
+	public String getEmailTo() {
+		return emailTo;
+	}
+
+	public void setEmailTo(String emailTo) {
+		this.emailTo = emailTo;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public LocalDateTime getSendDateEmail() {
+		return sendDateEmail;
+	}
+
+	public void setSendDateEmail(LocalDateTime sendDateEmail) {
+		this.sendDateEmail = sendDateEmail;
+	}
+
+	public StatusEmail getStatusEmail() {
+		return statusEmail;
+	}
+
+	public void setStatusEmail(StatusEmail statusEmail) {
+		this.statusEmail = statusEmail;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(emailId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EmailModel other = (EmailModel) obj;
+		return Objects.equals(emailId, other.emailId);
+	}
+	
+	
+}
